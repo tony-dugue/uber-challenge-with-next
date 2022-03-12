ca8efee87750f179fd6068d91b0210e621584eb3
tony dugué <duguetony@gmail.com>
2022-03-13 00:39:27 +0100

    Home => add Buttons section

--- pages/index.js
+++ pages/index.js
@@ -16,11 +16,27 @@ export default function Home() {
             <Name>Tony Dugué</Name>
             <UserImage src="/tony.jpeg"/>
           </Profile>
-
         </Header>
 
-        {/* ActionButtons */}
+        <ActionButtons>
+          <ActionButton>
+            <ActionButtonImage src="/uberxx.png" />
+            Voiturier
+          </ActionButton>
+
+          <ActionButton>
+            <ActionButtonImage src="/bike.png" />
+            2 roues
+          </ActionButton>
+
+          <ActionButton>
+            <ActionButtonImage src="/uberschedule.png" />
+            Réserver
+          </ActionButton>
+        </ActionButtons>
+
         {/* InputButton */}
+
       </ActionItems>
 
     </Wrapper>
@@ -29,10 +45,18 @@ export default function Home() {
 
 const Wrapper = tw.div`flex flex-col h-screen`
 const ActionItems = tw.div`flex-1 p-4`
+
 const Header = tw.div`flex justify-between items-center`
 const UberLogo = tw.img`h-28`
 const Profile = tw.div`flex items-center`
 const Name = tw.div`mr-4 w-20 text-sm`
 const UserImage = tw.img`h-12 w-12 rounded-full border border-gray-200 p-px`
+
+const ActionButtons = tw.div`flex`
+const ActionButton = tw.div`
+flex flex-col flex-1 items-center justify-center bg-gray-200 m-1 h-32 rounded-lg text-xl
+transform hover:scale-105 transition
+`
+const ActionButtonImage = tw.img`h-3/5`
 
 
