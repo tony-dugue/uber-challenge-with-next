badd2bf52127baa1c18d61a589e3bb14f001edd6
tony dugué <duguetony@gmail.com>
2022-03-13 00:05:04 +0100

    add Header section

--- pages/index.js
+++ pages/index.js
@@ -4,14 +4,35 @@ import Map from './components/Map'
 export default function Home() {
   return (
     <Wrapper>
+
       <Map />
-      <ActionItems>Start</ActionItems>
+
+      <ActionItems>
+
+        <Header>
+          <UberLogo src="/uber-logo.jpeg" />
+
+          <Profile>
+            <Name>Tony Dugué</Name>
+            <UserImage src="/tony.jpeg"/>
+          </Profile>
+
+        </Header>
+
+        {/* ActionButtons */}
+        {/* InputButton */}
+      </ActionItems>
+
     </Wrapper>
   )
 }
 
 const Wrapper = tw.div`flex flex-col h-screen`
-
-const ActionItems = tw.div`flex-1`
+const ActionItems = tw.div`flex-1 p-4`
+const Header = tw.div`flex justify-between items-center`
+const UberLogo = tw.img`h-28`
+const Profile = tw.div`flex items-center`
+const Name = tw.div`mr-4 w-20 text-sm`
+const UserImage = tw.img`h-12 w-12 rounded-full border border-gray-200 p-px`
 
 
